6f2a574808207b64e3bbee4bd58baf276bd22105
Luc Patiny <luc@patiny.com>
2016-06-30 15:00:16 +0200

    Add possibility to add a SVG in toDaistereotopicSVG to allow testing

--- src/extend/toDiastereotopicSVG.js
+++ src/extend/toDiastereotopicSVG.js
@@ -5,8 +5,9 @@ module.exports = function toDiastereotopicSVG(options) {
     var width=options.width||300;
     var height=options.width||200;
     var prefix=options.width||"ocl";
+    var svg=options.svg;
     var diaIDs = this.getDiastereotopicAtomIDs();
-    var svg=this.toSVG(width, height, prefix);
+    if (!svg) svg=this.toSVG(width, height, prefix);
 
     svg=svg.replace(/Atom:[0-9]+\"/g,function(value) {
         var atom=value.replace(/[^0-9]/g,'');
