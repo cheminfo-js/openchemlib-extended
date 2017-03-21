2f862122b0e81b16f5a8169451139c2ca092ac97
Daniel Kostro <kostro.d@gmail.com>
2017-03-21 15:57:20 +0100

    Revert "toSVG options"
    This reverts commit 36d7116a9567c67cc3a2ce53f654fb836c3ababd.

--- src/extend/toSVG.js
+++ src/extend/toSVG.js
@@ -3,16 +3,11 @@
 
 module.exports = function (OCL) {
     const originalToSVG = OCL.Molecule.prototype.toSVG;
-    return function (width, height, id, options) {
-        options = options || {};
-        let svg = originalToSVG.call(this, width, height, id, options);
+    return function (width, height, options={}) {
 
-        if (options.bold) {
-            svg = svg.replace(/font-family=" Helvetica" /g, 'font-family=" Helvetica" font-weight="bold" ');
-        }
-        if (options.strokeWidth) {
-            svg = svg.replace(/stroke-width:1/g, `stroke-width:${options.strokeWidth}`);
-        }
-        return svg;
+        const svg = originalToSVG.call(this, width, height);
+
+        return svg.replace(/font-family=" Helvetica" /g, 'font-family=" Helvetica" font-weight="bold" ')
+        .replace(/stroke-width:1/g, 'stroke-width:2');
     };
 };
--- test/extend/toSVG.js
+++ test/extend/toSVG.js
@@ -1,21 +1,18 @@
 'use strict';
 
+
 var OCLE = require('../..');
 
-describe('toSVG test', function () {
-    it('check chlorobenzene', function () {
-        let molecule = OCLE.Molecule.fromSmiles('c1ccccc1Cl');
-        let svg = molecule.toSVG(200, 200);
-        svg.indexOf('stroke-width:1').should.be.above(0);
-        svg.indexOf('Cl').should.be.above(0);
-        svg.indexOf('font-weight="bold"').should.be.equal(-1);
 
-        svg = molecule.toSVG(200, 200, null, {
-            strokeWidth: 2,
-            bold: true
-        });
+describe('toSVG test', function () {
+    it('check benzene', function () {
+        var molecule=OCLE.Molecule.fromSmiles('c1ccccc1Cl');
+        var svg=molecule.toSVG(200, 200);
         svg.indexOf('stroke-width:2').should.be.above(0);
         svg.indexOf('Cl').should.be.above(0);
         svg.indexOf('font-weight="bold"').should.be.above(0);
     });
+
+    
+
 });
