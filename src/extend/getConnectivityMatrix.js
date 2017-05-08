2d58b048093158ffe256175abe256258f44a2b87
jobo322 <jose.bolanos@correounivalle.edu.co>
2017-05-08 09:39:30 +0200

    added options pathLength to getConnectivity

--- src/extend/getConnectivityMatrix.js
+++ src/extend/getConnectivityMatrix.js
@@ -1,10 +1,15 @@
 'use strict';
+var floydWarshall = require('ml-floyd-warshall');
+var Matrix = require('ml-matrix');
 
 module.exports = function (OCL) {
     return function getConnectivityMatrix(options = {}) {
-        var sdt = options.sdt;
-        var mass = options.mass;
-        var atomicNo = options.atomicNo;
+
+        if (!options.pathLength) {
+            var sdt = options.sdt;
+            var mass = options.mass;
+            var atomicNo = options.atomicNo;
+        }
 
         this.ensureHelperArrays(OCL.Molecule.cHelperNeighbours);
         var nbAtoms = this.getAllAtoms();
@@ -24,6 +29,11 @@ module.exports = function (OCL) {
                 result[i][this.getConnAtom(i, j)] = (sdt) ? this.getConnBondOrder(i, j) : 1;
             }
         }
+
+        if (options.pathLength) {
+            result = floydWarshall(new Matrix(result));
+        }
+
         return result;
     };
 };
