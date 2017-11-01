62cc01ed5e4a898d30bc09e9015fc812f98a674d
Luc Patiny <luc@patiny.com>
2017-11-01 16:42:38 +0100

    add option to get SVG with heavy atom diaID

--- src/extend/toDiastereotopicSVG.js
+++ src/extend/toDiastereotopicSVG.js
@@ -4,17 +4,43 @@ module.exports = function toDiastereotopicSVG(options = {}) {
     var {
         width = 300,
         height = 200,
-        prefix = 'ocl'
+        prefix = 'ocl',
+        heavyAtomHydrogen = false
 
     } = options;
     var svg = options.svg;
-    var diaIDs = this.getDiastereotopicAtomIDs();
+    var diaIDs = [];
+
+    var hydrogenInfo = {};
+    this.getExtendedDiastereotopicAtomIDs().forEach(function (line) {
+        hydrogenInfo[line.oclID] = line;
+    });
+
+    if (heavyAtomHydrogen) {
+        for (let i = 0; i < this.getAtoms(); i++) {
+            diaIDs.push([]);
+        }
+        let groupedDiaIDs = this.getGroupedDiastereotopicAtomIDs();
+        groupedDiaIDs.forEach(function (diaID) {
+            if (hydrogenInfo[diaID.oclID] && hydrogenInfo[diaID.oclID].nbHydrogens > 0) {
+                diaID.atoms.forEach(atom => {
+                    hydrogenInfo[diaID.oclID].hydrogenOCLIDs.forEach((id) => {
+                        if (!diaIDs[atom * 1].includes(id)) diaIDs[atom].push(id);
+                    });
+                });
+            }
+        });
+    } else {
+        diaIDs = this.getDiastereotopicAtomIDs().map(a => [a]);
+    }
+
     if (!svg) svg = this.toSVG(width, height, prefix);
 
     svg = svg.replace(/Atom:[0-9]+\"/g, function (value) {
         var atom = value.replace(/[^0-9]/g, '');
-        return value + ' data-atomid="' + diaIDs[atom] + '"';
+        return value + ' data-atomid="' + diaIDs[atom].join(',') + '"';
     });
 
     return svg;
 };
+
