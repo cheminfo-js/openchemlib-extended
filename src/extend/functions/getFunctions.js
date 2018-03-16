ab862e476ff82e0e319d0ae5c72c9216553d74d2
Luc Patiny <luc@patiny.com>
2018-03-16 06:58:51 +0100

    working on bond fragmentation

---
+++ src/extend/functions/getFunctions.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var functionIndex = require('./functions');
+
+module.exports = function getFunctions() {
+  var currentFunctionCodes = this.getFunctionCodes();
+  var currentFunctions = [];
+  for (var fragment of currentFunctionCodes) {
+    if (functionIndex[fragment.idCode]) {
+      var currentFunction = JSON.parse(JSON.stringify(functionIndex[fragment.idCode]));
+      currentFunction.atomMap = fragment.atomMap;
+      currentFunctions.push(currentFunction);
+    }
+  }
+  return currentFunctions;
+};
---
+++ src/extend/getHoseCodesForAtom.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// This is a javascript COPY of the java class !!!!
+
+
+module.exports = function (OCL) {
+  return function getHoseCodesForAtom(originalMolecule, rootAtom, options = {}) {
+    let FULL_HOSE_CODE = 1;
+    let HOSE_CODE_CUT_C_SP3_SP3 = 2;
+    const {
+      minSphereSize = 0,
+      maxSphereSize = 4,
+      kind = FULL_HOSE_CODE,
+    } = options;
+
+    let molecule = originalMolecule.getCompactCopy();
+
+    molecule.setAtomCustomLabel(rootAtom, `${molecule.getAtomLabel(rootAtom)}*`);
+    molecule.setAtomicNo(rootAtom, OCL.Molecule.getAtomicNoFromLabel('X'));
+
+    let fragment = new OCL.Molecule();
+    let results = [];
+    let min = minSphereSize;
+    let max = 0;
+    let atomMask = new Array(molecule.getAllAtoms());
+    let atomList = new Array(molecule.getAllAtoms());
+    for (var sphere = 0; sphere < maxSphereSize; sphere++) {
+      if (max === 0) {
+        atomList[0] = rootAtom;
+        atomMask[rootAtom] = true;
+        max = 1;
+      } else {
+        let newMax = max;
+        for (let i = min; i < max; i++) {
+          let atom = atomList[i];
+          for (let j = 0; j < molecule.getConnAtoms(atom); j++) {
+            let connAtom = molecule.getConnAtom(atom, j);
+            if (!atomMask[connAtom]) {
+              switch (kind) {
+                case FULL_HOSE_CODE:
+                  atomMask[connAtom] = true;
+                  atomList[newMax++] = connAtom;
+                  break;
+                case HOSE_CODE_CUT_C_SP3_SP3:
+                  if (!(isCsp3(mol, atom) && isCsp3(mol, connAtom))) {
+                    atomMask[connAtom] = true;
+                    atomList[newMax++] = connAtom;
+                  }
+                  break;
+                default:
+                  throw new Error('getHoseCoesForAtom unknown kind');
+              }
+            }
+          }
+        }
+        min = max;
+        max = newMax;
+      }
+
+      molecule.copyMoleculeByAtoms(fragment, atomMask, true, null);
+
+	    results.push(fragment.getCanonizedIDCode(OCL.Molecule.CANONIZER_ENCODE_ATOM_CUSTOM_LABELS));
+    }
+    return results;
+  };
+};
