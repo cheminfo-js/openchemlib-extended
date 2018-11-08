08d6e51d28880a48e1729b625fa7e008fa3a561b
Luc Patiny <luc@patiny.com>
2018-11-08 21:59:12 +0100

    add getBoundary

---
+++ src/extend/__tests__/getBoundary.test.js
@@ -0,0 +1,13 @@
+'use strict';
+const fs = require('fs');
+const join = require('path').join;
+
+const OCLE = require('../..');
+
+
+test('get the boundary of a molfile', () => {
+  let molfile = fs.readFileSync(join(__dirname, 'ru.mol'), 'utf8');
+  let molecule = OCLE.Molecule.fromMolfile(molfile);
+
+  expect(molecule.getBoundary()).toEqual({ depth: 4.956, height: 4.956, maxX: 13.1799, maxY: -0, maxZ: -0, minX: 0, minY: -4.956, minZ: -4.956, width: 13.1799 });
+});
---
+++ src/extend/getBoundary.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/**
+ * @returns {object} : minX, maxX, minY, maxY, minZ, maxZ, width, height, depth
+ */
+
+module.exports = function () {
+  return function getBoundary() {
+    let minX = Number.POSITIVE_INFINITY;
+    let maxX = Number.NEGATIVE_INFINITY;
+    let minY = Number.POSITIVE_INFINITY;
+    let maxY = Number.NEGATIVE_INFINITY;
+    let minZ = Number.POSITIVE_INFINITY;
+    let maxZ = Number.NEGATIVE_INFINITY;
+
+    for (let i = 0; i < this.getAllAtoms(); i++) {
+      let x = this.getAtomX(i);
+      let y = this.getAtomY(i);
+      let z = this.getAtomZ(i);
+      minX = Math.min(minX, x);
+      maxX = Math.max(maxX, x);
+      minY = Math.min(minY, y);
+      maxY = Math.max(maxY, y);
+      minZ = Math.min(minY, z);
+      maxZ = Math.max(maxY, z);
+    }
+
+    return {
+      minX,
+      maxX,
+      maxY,
+      minY,
+      maxZ,
+      minZ,
+      width: maxX - minX,
+      height: maxY - minY,
+      depth: maxZ - minZ
+    };
+  };
+};
